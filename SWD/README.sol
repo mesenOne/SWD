adc08d991ef176534504fa4be4e61892c498a2de
mesenOne <1090544968@qq.com>
2018-04-26 17:29:21 +0800

    first commit

---
+++ SWD/README.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.4.17;
+
+import "./MintableToken.sol";
+import "./PausableToken.sol";
+
+/**
+ * @dev Swd token ERC20 contract
+ * Based on references from OpenZeppelin: https://github.com/OpenZeppelin/zeppelin-solidity
+ */
+contract Swd is MintableToken, PausableToken {
+    string public constant version = "1.1";
+    string public constant name = "Swd Crypto Platform";
+    string public constant symbol = "Swd";
+    uint8 public constant decimals = 18;
+
+    event MintMasterTransferred(address indexed previousMaster, address indexed newMaster);
+
+    address public mintMaster;
+
+    modifier onlyMintMasterOrOwner() {
+        require(msg.sender == mintMaster || msg.sender == owner);
+        _;
+    }
+
+    function Swd() public {
+        mintMaster = msg.sender;
+    }
+
+    function transferMintMaster(address newMaster) onlyOwner public {
+        require(newMaster != address(0));
+        MintMasterTransferred(mintMaster, newMaster);
+        mintMaster = newMaster;
+    }
+
+    function mintToAddresses(address[] addresses, uint256 amount) public onlyMintMasterOrOwner canMint {
+        for (uint i = 0; i < addresses.length; i++) {
+            require(mint(addresses[i], amount));
+        }
+    }
+
+    function mintToAddressesAndAmounts(address[] addresses, uint256[] amounts) public onlyMintMasterOrOwner canMint {
+        require(addresses.length == amounts.length);
+        for (uint i = 0; i < addresses.length; i++) {
+            require(mint(addresses[i], amounts[i]));
+        }
+    }
+    /**
+     * @dev Function to mint tokens
+     * @param _to The address that will receive the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address _to, uint256 _amount) onlyMintMasterOrOwner canMint public returns (bool) {
+
+
+        address oldOwner = owner;
+        owner = msg.sender;
+        bool result = super.mint(_to, _amount);
+        owner = oldOwner;
+        return result;
+    }
+
+
+
+
+}
